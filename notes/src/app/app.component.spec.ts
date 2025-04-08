e5e341e5bed1555377f2994047eab3b03773c887
Alexandre ANSTETT <alexandre.anstett@etu.u-bourgogne.fr>
2025-04-08 14:01:35 +0200

    Debut des routes

--- notes/src/app/app.component.spec.ts
+++ notes/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'notes' title`, () => {
+  it(`should have the 'Alexandre ANSTETT' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('notes');
+    expect(app.title).toEqual('Alexandre ANSTETT');
   });
 
-  it('should render title', () => {
+  it('should render Alexandre ANSTETT', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, notes');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Alexandre ANSTETT');
   });
 });
