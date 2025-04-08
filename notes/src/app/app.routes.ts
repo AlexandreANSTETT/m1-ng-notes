7214075330d5cdd13e37dd15852cc7e9aede9fb5
Alexandre ANSTETT <alexandre.anstett@etu.u-bourgogne.fr>
2025-04-08 14:19:27 +0200

    Retour aux tags

--- notes/src/app/app.routes.ts
+++ notes/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { TagsComponent } from './tags/tags.component';
 
 export const routes: Routes = [
     {path: "tags" , component: TagsComponent},
+    {path: "" , redirectTo: "tags" , pathMatch: "full"},
 ];
 
 
